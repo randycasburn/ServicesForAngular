052acbc4b2f430ca872691cbd107df86c67c6d67
Randy Casburn <randy@plurious.com>
2019-07-20 17:55:34 -0400

    batch file fixes - taking longer than writing the service!

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
-# This file assumes that maven is installed
+REM This file assumes that maven is installed
+@echo off
 echo The server resides on port 80
-mvn install; mvn spring-boot:run; start "" http://localhost
+mvn install
+mvn spring-boot:run
