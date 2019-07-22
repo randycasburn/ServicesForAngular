bb5a21240efac35f758994e8e2e5d52d3015d36e
Randy Casburn <randy@plurious.com>
2019-07-22 12:08:55 -0400

    Added Cors

--- src/main/java/com/fidelity/servicesforangular/endpoints/CarsController.java
+++ src/main/java/com/fidelity/servicesforangular/endpoints/CarsController.java
@@ -4,14 +4,12 @@ package com.fidelity.servicesforangular.endpoints;
 import com.fidelity.servicesforangular.models.Car;
 import com.fidelity.servicesforangular.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/cars")
 public class CarsController {
     @Autowired
